f7d4602b6546c0011e0b5d3da84ba87468c82874
Varunraje <varunraje47@gmail.com>
2023-04-12 13:18:08 +0530

    recipe page

---
+++ pages/recipe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const recipe = () => {
+  return (
+    <>
+      <div className='px-44'>
+        <p className='text-2xl font-semibold'>Recipes News</p>
+        <div className='mt-2 space-y-4 text-base'>
+          <h1>Donec in metus lectus. Integer vulputate porta elit, fringilla mollis mag luctus vel. Interdum et malesuada fames ac ante ipsum primis in fauci. Pellentesque in aliquam enim, quis lobortis arcu. Curabitur quis ultrices est. Donec dapibus felis vel venenatis consequat. Sed feugiat diam consequat velit un fermentum turpis cursus ante ipsum primis in fauci.</h1>
+          <p>Cras semper, neque vitae hendrerit venenatis, tortor erat faucibus justo, sed faucibus orci felis vitae risus.Donec in metus lectus. Integer vulputate porta elit, fringilla mollis mag luctus vel. Interdum et malesuada fames ac ante ipsum primis in fauci. Pellentesque in aliquam enim, quis lobortis arcu. Curabitur quis ultrices est. Donec dapibus felis vel venenatis consequat. Sed feugiat diam consequat velit un fermentum turpis cursus ante ipsum primis in fauci.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default recipe
